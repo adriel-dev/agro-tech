87d284342550510b84d90c1d99c364ea55b56549
Adriel Felix <adrielandrade3@gmail.com>
2023-10-03 09:24:52 -0300

    import fix

--- agro-tech-domain/src/main/kotlin/br/com/agrotech/domain/breed/port/api/usecase/FindBreedById.kt
+++ agro-tech-domain/src/main/kotlin/br/com/agrotech/domain/breed/port/api/usecase/FindBreedById.kt
@@ -1,6 +1,7 @@
 package br.com.agrotech.domain.breed.port.api.usecase
 
 import br.com.agrotech.domain.breed.model.Breed
+import java.util.*
 
 interface FindBreedById {
     fun find(breedId: UUID): Breed
