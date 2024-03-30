fc9b9e02dacdd486a6aa4b6b799705699d6bcc6d
Adriel Felix <adrielandrade3@gmail.com>
2024-03-30 02:23:08 -0300

    task web adapter: controller, dtos, converter, beans

---
+++ agro-tech-infra/agro-tech-web/src/main/kotlin/br/com/agrotech/web/task/config/TaskUseCasesBeans.kt
@@ -0,0 +1,27 @@
+package br.com.agrotech.web.task.config
+
+import br.com.agrotech.domain.task.port.api.usecase.*
+import br.com.agrotech.domain.task.port.api.usecase.impl.*
+import br.com.agrotech.domain.task.port.spi.persistence.TaskRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class TaskUseCasesBeans {
+
+    @Bean
+    open fun findAllByStartDate(taskRepository: TaskRepository): FindAllTasksByStartDate = FindAllTasksByStartDateUseCase(taskRepository)
+
+    @Bean
+    open fun createTask(taskRepository: TaskRepository): CreateTask = CreateTaskUseCase(taskRepository)
+
+    @Bean
+    open fun markTaskAsDoing(taskRepository: TaskRepository): MarkTaskAsDoing = MarkTaskAsDoingUseCase(taskRepository)
+
+    @Bean
+    open fun markTaskAsDone(taskRepository: TaskRepository): MarkTaskAsDone = MarkTaskAsDoneUseCase(taskRepository)
+
+    @Bean
+    open fun deleteTaskById(taskRepository: TaskRepository): DeleteTaskById = DeleteTaskByIdUseCase(taskRepository)
+
+}
