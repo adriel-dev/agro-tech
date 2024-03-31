752e0141f53337650e1154e74b9ca1e7ce721289
Adriel Felix <adrielandrade3@gmail.com>
2024-03-30 21:01:48 -0300

    task start date validation

--- agro-tech-domain/src/main/kotlin/br/com/agrotech/domain/task/port/api/usecase/impl/CreateTaskUseCase.kt
+++ agro-tech-domain/src/main/kotlin/br/com/agrotech/domain/task/port/api/usecase/impl/CreateTaskUseCase.kt
@@ -1,5 +1,6 @@
 package br.com.agrotech.domain.task.port.api.usecase.impl
 
+import br.com.agrotech.domain.task.exception.InvalidStartDateException
 import br.com.agrotech.domain.task.model.Task
 import br.com.agrotech.domain.task.port.api.usecase.CreateTask
 import br.com.agrotech.domain.task.port.spi.persistence.TaskRepository
@@ -9,6 +10,11 @@ class CreateTaskUseCase(
 ) : CreateTask {
 
     override fun create(task: Task): Task {
+        val startDate = task.startDate!!
+        val finishUntil = task.finishUntil!!
+        if(startDate.isAfter(finishUntil)) {
+            throw InvalidStartDateException(startDate, finishUntil)
+        }
         return taskRepository.saveTask(task)
     }
 
